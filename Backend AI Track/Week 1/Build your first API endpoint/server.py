93726afa25360cf22f6fccb62f11b4d503305686
LCoder-FILE <emonphil46@gmail.com>
2026-07-11 22:38:23 +0700

    completed server.py

--- Backend AI Track/Week 1/Build your first API endpoint/server.py
+++ Backend AI Track/Week 1/Build your first API endpoint/server.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"message" : "the server is on"})
+
+@app.route("/greeting")
+def greeting():
+    return jsonify({"message" : "hello from Indonesia !!!"})
+
+
+if __name__ == "__main__":
+    app.run()
+
+    # to execute : python ./"Backend AI Track"/"Week 1"/"Build your first API endpoint"/server.py
+    # to see with curl : curl http://127.0.0.1:5000 (for {"message" : "the server is on"}) 
+    #                    curl http://127.0.0.1:5000/greeting (for {"message" : "hello from Indonesia !!!"})
+
